2da61429b385306f9dd868f93399fde69ae5d417
Janos Gyerik <janos@kronos>
2017-07-15 20:13:10 +0200

    Add files

---
+++ spec/atom-dummy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDummyView from '../lib/atom-dummy-view';
+
+describe('AtomDummyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
